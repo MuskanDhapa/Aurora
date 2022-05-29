9cfa2ad10f7bc93954a472cb7f0a4208e508672a
NipunSharma2902 <sharmanipun2902@gmail.com>
2022-05-29 22:18:49 +0530

    e

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,6 @@
 import nltk
 nltk.download('omw-1.4')
-#nltk.download('punkt')
+nltk.download(punkt)
 nltk.download('wordnet')
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
