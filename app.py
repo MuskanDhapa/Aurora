9d081f276ce63c16211f20a08300a91f05c79489
NipunSharma2902 <sharmanipun2902@gmail.com>
2022-05-29 17:04:54 +0530

    This time?

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 
 #app.config['SECRET_KEY'] = 'enter-a-very-secretive-key-3479373'
 
-
+@app.route('/', methods=["GET", "POST"])
+def index():
+    return render_template('index.html', **locals())
 
 
 
