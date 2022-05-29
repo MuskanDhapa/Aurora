07975601c0b8bc154436ad80b432278b3a8b3308
NipunSharma2902 <sharmanipun2902@gmail.com>
2022-05-29 13:57:50 +0530

    Now heroku deployment

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ import processor
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'enter-a-very-secretive-key-3479373'
+#app.config['SECRET_KEY'] = 'enter-a-very-secretive-key-3479373'
 
 
 @app.route('/', methods=["GET", "POST"])
