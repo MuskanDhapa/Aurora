12c1718db7c0f719676f5a029b37bdf3e5e0b87a
NipunSharma2902 <sharmanipun2902@gmail.com>
2022-05-29 21:58:27 +0530

    .

--- processor.py
+++ processor.py
@@ -2,6 +2,7 @@ import nltk
 nltk.download('omw-1.4')
 nltk.download('punkt')
 nltk.download('wordnet')
+nltk.download('english')
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 import pickle
